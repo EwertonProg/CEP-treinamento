3635cf5cce66df0cf6c4b78859944248d94a702c
Ewerton Luis Santana <@eljsantana>
2021-04-13 20:56:35 -0300

    init commit

---
+++ app/src/main/java/com/ewerton/cep/Cep.kt
@@ -0,0 +1,10 @@
+package com.ewerton.cep
+
+data class Cep(
+    var cep: String,
+    var logradouro: String,
+    var complemento: String,
+    var bairro: String,
+    var localidade: String,
+    var uf: String
+)
---
+++ app/src/main/java/com/ewerton/cep/CepService.kt
@@ -0,0 +1,10 @@
+package com.ewerton.cep
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CepService {
+    @GET("{cep}/json/")
+    fun getCep(@Path("cep") cep: String): Call<Cep>
+}
